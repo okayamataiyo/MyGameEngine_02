8914974a73d94d9c016def5f012e211fb68fff94
太陽 岡山 <220069@jc-21.jp>
2023-05-30 11:33:15 +0900

    コンストバッファ作成

---
+++ Quad.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <DirectXMath.h>
+#include "Direct3D.h"
+
+using namespace DirectX;
+
+//コンスタントバッファー
+struct CONSTANT_BUFFER
+{
+	XMMATRIX	matWVP;
+};
+
+class Quad
+{
+	//メンバ変数
+	ID3D11Buffer* pVertexBuffer_;	//頂点バッファ
+	ID3D11Buffer* pIndexBuffer_;	//インデックスバッファ
+	ID3D11Buffer* pConstantBuffer_;	//コンスタントバッファ
+public:
+	//メンバ関数
+	Quad();
+	~Quad();
+	void Initialize();
+	void Draw();
+	void Release();
+};
+
