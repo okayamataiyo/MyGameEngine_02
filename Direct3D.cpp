f653c9b5531328a995fcd1fd457b777f9a3b6243
太陽 岡山 <220069@jc-21.jp>
2023-06-06 12:31:42 +0900

    Cameraクラスを作成し、描画に成功した。

--- Direct3D.cpp
+++ Direct3D.cpp
@@ -18,8 +18,9 @@ namespace Direct3D
 
 
 //初期化
-void Direct3D::Initialize(int winW, int winH, HWND hWnd)
+HRESULT Direct3D::Initialize(int winW, int winH, HWND hWnd)
 {
+	HRESULT hr;
 	///////////////////////////いろいろ準備するための設定///////////////////////////////
 	//いろいろな設定項目をまとめた構造体
 	DXGI_SWAP_CHAIN_DESC scDesc;
@@ -46,7 +47,7 @@ void Direct3D::Initialize(int winW, int winH, HWND hWnd)
 
 	////////////////上記設定をもとにデバイス、コンテキスト、スワップチェインを作成////////////////////////
 	D3D_FEATURE_LEVEL level;
-	D3D11CreateDeviceAndSwapChain(
+	hr = D3D11CreateDeviceAndSwapChain(
 	nullptr,					// どのビデオアダプタを使用するか？既定ならばnullptrで
 	D3D_DRIVER_TYPE_HARDWARE,	// ドライバのタイプを渡す。ふつうはHARDWARE
 	nullptr,					// 上記をD3D_DRIVER_TYPE_SOFTWAREに設定しないかぎりnullptr
@@ -63,15 +64,21 @@ void Direct3D::Initialize(int winW, int winH, HWND hWnd)
 	///////////////////////////レンダーターゲットビュー作成///////////////////////////////
 	//スワップチェーンからバックバッファを取得（バックバッファ ＝ レンダーターゲット）
 	ID3D11Texture2D* pBackBuffer;
-	pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
-
-	HRESULT hr;
+	hr = pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+	if (FAILED(hr))
+	{
+		//エラー処理,早期リターン
+		OutputDebugString("スワップチェーンからバックバッファを取得に失敗");
+		return hr;
+	}
 
 	//レンダーターゲットビューを作成
 	hr = pDevice->CreateRenderTargetView(pBackBuffer, NULL, &pRenderTargetView);
-	if (hr == E_FAIL)
+	if(FAILED(hr))
 	{
 		//失敗したときの処理
+		OutputDebugString("レンダーターゲットビュー作成に失敗");
+		return hr;
 	}
 
 	//一時的にバックバッファを取得しただけなので解放
@@ -98,23 +105,45 @@ void Direct3D::Initialize(int winW, int winH, HWND hWnd)
 	ID3D11RenderTargetView* pRenderTargetView;	//レンダーターゲットビュー
 
 	//シェーダー準備
-	InitShader();
+	hr = InitShader();
+	if (FAILED(hr))
+	{
+		//エラー処理,早期リターン
+		OutputDebugString("シェーダ準備に失敗");
+		return hr;
+	}
+
+	return S_OK;
 }
 
 //シェーダー準備
-void Direct3D::InitShader()
+HRESULT Direct3D::InitShader()
 {
+	HRESULT hr;
 	// 頂点シェーダの作成（コンパイル）
 	ID3DBlob* pCompileVS = nullptr;
 	D3DCompileFromFile(L"Simple3D.hlsl", nullptr, nullptr, "VS", "vs_5_0", NULL, 0, &pCompileVS, NULL);
 	assert(pCompileVS != nullptr);
-	pDevice->CreateVertexShader(pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(), NULL, &pVertexShader);
-	
+
+	hr = pDevice->CreateVertexShader(pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(), NULL, &pVertexShader);
+	if (FAILED(hr))
+	{
+		//エラー処理,早期リターン
+		OutputDebugString("頂点シェーダーの作成に失敗");
+		return hr;
+	}
+
 	//頂点インプットレイアウト
 	D3D11_INPUT_ELEMENT_DESC layout[] = {
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0,  D3D11_INPUT_PER_VERTEX_DATA, 0 },	//位置
 	};
-	pDevice->CreateInputLayout(layout, 1, pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(), &pVertexLayout);
+	hr = pDevice->CreateInputLayout(layout, 1, pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(), &pVertexLayout);
+	if (FAILED(hr))
+	{
+		//エラー処理,早期リターン
+		OutputDebugString("頂点インプットレイアウトに失敗");
+		return hr;
+	}
 
 //	pCompileVS->Release();
 	SAFE_RELEASE(pCompileVS);
@@ -123,7 +152,13 @@ void Direct3D::InitShader()
 	ID3DBlob* pCompilePS = nullptr;
 	D3DCompileFromFile(L"Simple3D.hlsl", nullptr, nullptr, "PS", "ps_5_0", NULL, 0, &pCompilePS, NULL);
 	assert(pCompilePS != nullptr);
-	pDevice->CreatePixelShader(pCompilePS->GetBufferPointer(), pCompilePS->GetBufferSize(), NULL, &pPixelShader);
+	hr = pDevice->CreatePixelShader(pCompilePS->GetBufferPointer(), pCompilePS->GetBufferSize(), NULL, &pPixelShader);
+	if (FAILED(hr))
+	{
+		//エラー処理,早期リターン
+		OutputDebugString("ピクセルシェーダの作成に失敗");
+		return hr;
+	}
 //	pCompilePS->Release();
 	SAFE_RELEASE(pCompilePS);
 
@@ -132,13 +167,21 @@ void Direct3D::InitShader()
 	rdc.CullMode = D3D11_CULL_NONE;
 	rdc.FillMode = D3D11_FILL_SOLID;
 	rdc.FrontCounterClockwise = TRUE;
-	pDevice->CreateRasterizerState(&rdc, &pRasterizerState);
+	hr = pDevice->CreateRasterizerState(&rdc, &pRasterizerState);
+	if (FAILED(hr))
+	{
+		//エラー処理,早期リターン
+		OutputDebugString("ラスタライザ作成に失敗");
+		return hr;
+	}
 
 	//それぞれをデバイスコンテキストにセット
 	pContext->VSSetShader(pVertexShader, NULL, 0);	//頂点シェーダー
 	pContext->PSSetShader(pPixelShader, NULL, 0);	//ピクセルシェーダー
 	pContext->IASetInputLayout(pVertexLayout);	//頂点インプットレイアウト
 	pContext->RSSetState(pRasterizerState);		//ラスタライザー
+
+	return S_OK;
 }
 
 //描画開始
--- Quad.cpp
+++ Quad.cpp
@@ -1,4 +1,5 @@
 #include "Quad.h"
+#include "Camera.h"
 
 Quad::Quad():
 	pVertexBuffer_(nullptr),pIndexBuffer_(nullptr), pConstantBuffer_(nullptr)
@@ -11,8 +12,9 @@ Quad::~Quad()
 	Release();
 }
 
-void Quad::Initialize()
+HRESULT Quad::Initialize()
 {
+	HRESULT hr;
 	// 頂点情報
 	XMVECTOR vertices[] = //[4]でも同じ意味
 	{
@@ -33,7 +35,14 @@ void Quad::Initialize()
 	bd_vertex.StructureByteStride = 0;
 	D3D11_SUBRESOURCE_DATA data_vertex;
 	data_vertex.pSysMem = vertices;
-	Direct3D::pDevice->CreateBuffer(&bd_vertex, &data_vertex, &pVertexBuffer_);
+
+	hr = Direct3D::pDevice->CreateBuffer(&bd_vertex, &data_vertex, &pVertexBuffer_);
+	if (FAILED(hr))
+	{
+		//エラー処理,早期リターン
+		OutputDebugString("頂点データ用バッファの設定に失敗");
+		return hr;
+	}
 
 	//インデックス情報
 	int index[] = { 0,2,3, 0,1,2,};// 0, 4, 1};
@@ -50,7 +59,13 @@ void Quad::Initialize()
 	InitData.pSysMem = index;
 	InitData.SysMemPitch = 0;
 	InitData.SysMemSlicePitch = 0;
-	Direct3D::pDevice->CreateBuffer(&bd, &InitData, &pIndexBuffer_);
+	hr = Direct3D::pDevice->CreateBuffer(&bd, &InitData, &pIndexBuffer_);
+	if (FAILED(hr))
+	{
+		//エラー処理,早期リターン
+		OutputDebugString("インデックスバッファの作成に失敗");
+		return hr;
+	}
 
 	//コンスタントバッファ作成
 	D3D11_BUFFER_DESC cb;
@@ -62,21 +77,30 @@ void Quad::Initialize()
 	cb.StructureByteStride = 0;
 
 	// コンスタントバッファの作成
-	Direct3D::pDevice->CreateBuffer(&cb, nullptr, &pConstantBuffer_);
+	hr = Direct3D::pDevice->CreateBuffer(&cb, nullptr, &pConstantBuffer_);
+	if (FAILED(hr))
+	{
+		//エラー処理,早期リターン
+		OutputDebugString("コンストラクタバッファの作成に失敗");
+		return hr;
+	}
+	
+	return S_OK;
 }
 
 void Quad::Draw()
 {
 	//コンスタントバッファに渡す情報
-	XMVECTOR position = { 0, 3, -10, 0 };	//カメラの位置
-	XMVECTOR target = { 0, 0, 0, 0 };		//カメラの焦点
-	XMMATRIX view = XMMatrixLookAtLH(position, target, XMVectorSet(0, 1, 0, 0));		//ビュー行列
-	XMMATRIX proj = XMMatrixPerspectiveFovLH(XM_PIDIV4, 800.0f / 600.0f, 0.1f, 100.0f);	//射影行列
+	
+	
+	
+	
 
-	CONSTANT_BUFFER cb;
-	cb.matWVP = XMMatrixTranspose(view * proj);
+
 
 	D3D11_MAPPED_SUBRESOURCE pdata;
+	CONSTANT_BUFFER cb;
+	cb.matWVP = XMMatrixTranspose(Camera::GetViewMatrix() * Camera::GetProjectionMatrix());
 	Direct3D::pContext->Map(pConstantBuffer_, 0, D3D11_MAP_WRITE_DISCARD, 0, &pdata);	//GPUからのデータアクセスを止める
 	memcpy_s(pdata.pData, pdata.RowPitch, (void*)(&cb), sizeof(cb));					//データを値を送る
 	Direct3D::pContext->Unmap(pConstantBuffer_, 0);										//再開
