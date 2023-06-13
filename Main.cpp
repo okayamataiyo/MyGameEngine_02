4045a54db6f2653786544d3dd4400f6a2946ea0f
太陽 岡山 <220069@jc-21.jp>
2023-06-13 10:50:04 +0900

    verticesの面6の誤字の修正

--- Main.cpp
+++ Main.cpp
@@ -104,7 +104,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, 
 
 			//ï`âÊèàóù
 			static float a = 0;
-			a += 0.001;
+			a += 0.01;
 			XMMATRIX matRY = XMMatrixRotationY(XMConvertToRadians(a));
 			XMMATRIX matRX = XMMatrixRotationX(XMConvertToRadians(-a));
 			XMMATRIX matRZ = XMMatrixRotationZ(XMConvertToRadians(a));
