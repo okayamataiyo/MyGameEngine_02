25eb53025a211a0481b5e5fa20ab7899495f7a5e
太陽 岡山 <220069@jc-21.jp>
2023-06-27 11:23:30 +0900

    Fbxフォルダ作成中

--- Fbx.h
+++ Fbx.h
@@ -3,6 +3,8 @@
 #include <d3d11.h>
 #include <fbxsdk.h>
 #include <string>
+#include "Direct3D.h"
+#include "Texture.h"
 #include "Transform.h"
 
 #pragma comment(lib, "LibFbxSDK-MD.lib")
@@ -23,16 +25,23 @@ class Fbx
 	};
 
 	int vertexCount_;	//頂点数	FBXファイルを扱うために必要になる変数を宣言する。
+	VERTEX vertices_;
 	int polygonCount_;	//ポリゴン数
 
 	//メンバ変数
-	ID3D11Buffer* pVertexBuffer_;
-	ID3D11Buffer* pIndexBuffer_;
-	ID3D11Buffer* pConstantBuffer_;
+	ID3D11Buffer* pVertexBuffer_;	//頂点バッファ
+	ID3D11Buffer* pIndexBuffer_;	//インデックスバッファ
+	ID3D11Buffer* pConstantBuffer_;	//コンスタントバッファ
+	Texture* pTexture_;
 
 public:
+	//メンバ関数
 	Fbx();
 	HRESULT Load(std::string fileName);
+	void InitVertex(fbxsdk::FbxMesh* mesh);
+	void InitIndex(fbxsdk::FbxMesh* mesh);
+	void InitConstantBuffer();
+	HRESULT CreateIndexBuffer();
 	void Draw(Transform& transform);
 	void Release();
 };
