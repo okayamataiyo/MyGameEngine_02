e81c82f5d3b8a520358a73b554ccb4df27aee0bc
太陽 岡山 <220069@jc-21.jp>
2023-06-27 11:23:30 +0900

    Fbxフォルダ作成中

--- Fbx.cpp
+++ Fbx.cpp
@@ -1,4 +1,12 @@
+#include "Camera.h"
 #include "Fbx.h"
+
+
+Fbx::Fbx()
+    :vertexCount_(0),polygonCount_(0),pTexture_(nullptr),
+    pVertexBuffer_(nullptr),pIndexBuffer_(nullptr),pConstantBuffer_(nullptr)
+{
+}
 
 HRESULT Fbx::Load(std::string fileName)
 {
@@ -25,7 +33,135 @@ HRESULT Fbx::Load(std::string fileName)
 
     polygonCount_ = mesh->GetPolygonCount();	//ポリゴンの数
 
+    InitVertex(mesh);		//頂点バッファ準備
+
+    InitIndex(mesh);		//インデックスバッファ準備
+
+    InitConstantBuffer();	//コンスタントバッファ準備
+
     //マネージャ開放
     pFbxManager->Destroy();
     return S_OK;
+}
+
+//頂点バッファ準備
+void Fbx::InitVertex(fbxsdk::FbxMesh* mesh)
+{
+    //頂点情報を入れる配列
+    VERTEX* vertices = new VERTEX[vertexCount_];
+
+    //全ポリゴン
+    for (DWORD poly = 0; poly < polygonCount_; poly++)
+    {
+        //3頂点分
+        for (int vertex = 0; vertex < 3; vertex++)
+        {
+            //調べる頂点の番号
+            int index = mesh->GetPolygonVertex(poly, vertex);
+
+            //頂点の位置
+            FbxVector4 pos = mesh->GetControlPointAt(index);
+            vertices[index].position = XMVectorSet((float)pos[0], (float)pos[1], (float)pos[2], 0.0f);
+        }
+    }
+
+    // 頂点バッファ作成
+    HRESULT hr;
+	D3D11_BUFFER_DESC bd_vertex;
+	bd_vertex.ByteWidth = sizeof(VERTEX) * vertexCount_;
+	bd_vertex.Usage = D3D11_USAGE_DEFAULT;
+	bd_vertex.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd_vertex.CPUAccessFlags = 0;
+	bd_vertex.MiscFlags = 0;
+	bd_vertex.StructureByteStride = 0;
+	D3D11_SUBRESOURCE_DATA data_vertex;
+	data_vertex.pSysMem = vertices;
+	hr = Direct3D::pDevice_->CreateBuffer(&bd_vertex, &data_vertex, &pVertexBuffer_);
+	if (FAILED(hr))
+	{
+		MessageBox(NULL, "頂点バッファの作成に失敗しました", "エラー", MB_OK);
+	}
+}
+
+//インデックスバッファ準備
+void Fbx::InitIndex(fbxsdk::FbxMesh* mesh)
+{
+    int* index = new int[polygonCount_ * 3];
+    int count = 0;
+
+    //全ポリゴン
+    for (DWORD poly = 0; poly < polygonCount_; poly++)
+    {
+        //3頂点分
+        for (DWORD vertex = 0; vertex < 3; vertex++)
+        {
+            index[count] = mesh->GetPolygonVertex(poly, vertex);
+            count++;
+        }
+    }
+}
+
+void Fbx::InitConstantBuffer()
+{
+    D3D11_BUFFER_DESC cb;
+    cb.ByteWidth = sizeof(CONSTANT_BUFFER);
+    cb.Usage = D3D11_USAGE_DYNAMIC;
+    cb.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+    cb.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+    cb.MiscFlags = 0;
+    cb.StructureByteStride = 0;
+    // コンスタントバッファの作成
+    HRESULT hr;
+    hr = Direct3D::pDevice_->CreateBuffer(&cb, nullptr, &pConstantBuffer_);
+    if (FAILED(hr))
+    {
+        MessageBox(NULL, "コンスタントバッファの作成に失敗しました", "エラー", MB_OK);
+    }
+}
+
+HRESULT Fbx::CreateIndexBuffer()
+{
+    //頂点情報を入れる配列
+    VERTEX* vertices = new VERTEX[polygonCount_];
+    // インデックスバッファを生成する
+    D3D11_BUFFER_DESC   bd;
+    bd.Usage = D3D11_USAGE_DEFAULT;
+    bd.ByteWidth = sizeof(int) * polygonCount_;
+    bd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+    bd.CPUAccessFlags = 0;
+    bd.MiscFlags = 0;
+
+    D3D11_SUBRESOURCE_DATA InitData;
+    InitData.pSysMem = vertices;
+    InitData.SysMemPitch = 0;
+    InitData.SysMemSlicePitch = 0;
+
+    HRESULT hr;
+    hr = Direct3D::pDevice_->CreateBuffer(&bd, &InitData, &pIndexBuffer_);
+    if (FAILED(hr))
+    {
+        MessageBox(NULL, "インデックスバッファの作成に失敗しました", "エラー", MB_OK);
+        return hr;
+    }
+    return S_OK;
+}
+
+
+//テクスチャをロード
+void Fbx::Draw(Transform& transform)
+{
+    pTexture_ = new Texture;
+    HRESULT hr;
+    hr = pTexture_->Load("Assets\\Oden_01.fbx");
+    if (FAILED(hr))
+    {
+        MessageBox(NULL, "テクスチャの作成に失敗しました", "エラー", MB_OK);
+    }
+}
+
+void Fbx::Release()
+{
+    SAFE_RELEASE(pConstantBuffer_);
+    SAFE_RELEASE(pIndexBuffer_);
+    SAFE_RELEASE(pVertexBuffer_);
 }
