accac0956f3307d7740bf219f04d4f0c13c03d36
太陽 岡山 <220069@jc-21.jp>
2023-06-30 11:50:39 +0900

    escキーを使えrうようにした。

--- Input.h
+++ Input.h
@@ -1,5 +1,16 @@
 #pragma once
-class Input
-{
-};
 
+#include <dInput.h>
+
+#pragma comment(lib, "dxguid.lib")
+#pragma comment(lib, "dInput8.lib")
+
+#define SAFE_RELEASE(p) if(p != nullptr){ p->Release(); p = nullptr;}
+
+namespace Input
+{
+	void Initialize(HWND hWnd);
+	void Update();
+	bool IsKey(int keyCode);
+	void Release();
+};
