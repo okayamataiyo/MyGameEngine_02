a497dc7ea3d409e348a693eb15325d36287ac4fa
太陽 岡山 <220069@jc-21.jp>
2023-07-12 13:00:29 +0900

    Modelクラスを追加

---
+++ Model.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<string>
+//必要なincludeを書く
+
+namespace Model
+{
+	struct ModelData {
+		//FBX
+		//トランスフォーム
+		//ファイル名
+	};
+	//モデルのポインタをぶち込んでおくベクタ
+};
+
+int Load(std::string fileName)
