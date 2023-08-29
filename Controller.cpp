c45feb2ed96bfc126c288dc25a06bca14cbf3f70
太陽 岡山 <220069@jc-21.jp>
2023-08-29 11:31:09 +0900

    カメラをWASD、↑→←↓で移動できるようにした。

--- Controller.cpp
+++ Controller.cpp
@@ -6,16 +6,11 @@
 
 enum
 {
-    CAM_TYPE_FIXED,      //固定
-    CAM_TYPE_TPS_NO_ROT, //三人称(回転無し)
-    CAM_TYPE_TPS,        //三人称(回転あり)
-    CAM_TYPE_FPS,        //一人称
-    CAM_TYPE_MAX,
 };
 
 //コンストラクタ
 Controller::Controller(GameObject* parent)
-    :GameObject(parent, "Controller"), hModel_(-1), camType_(CAM_TYPE_FIXED)
+    :GameObject(parent, "Controller"), hModel_(-1)
 {
 }
 
@@ -28,8 +23,10 @@ Controller::~Controller()
 void Controller::Initialize()
 {
     //モデルデータのロード
-    hModel_ = Model::Load("Assets/BoxGrass.fbx");
+    hModel_ = Model::Load("Assets/BoxWater.fbx");
     assert(hModel_ >= 0);
+
+    transform_.position_.y = -0.0001f;
 
     //Instantiate<ControllerHead>(this);
 }
@@ -37,17 +34,17 @@ void Controller::Initialize()
 //更新
 void Controller::Update()
 {
-    if (Input::IsKey(DIK_A))
-    {
-        //何らかの処理
-        transform_.rotate_.y += -1.0f;     // 1dd°ずつ回転
-    }
+    //if (Input::IsKey(DIK_A))
+    //{
+    //    //何らかの処理
+    //    transform_.rotate_.y += -1.0f;     // 1dd°ずつ回転
+    //}
 
-    if (Input::IsKey(DIK_D))
-    {
-        //何らかの処理
-        transform_.rotate_.y += 1.0f;     // 1°ずつ回転
-    }
+    //if (Input::IsKey(DIK_D))
+    //{
+    //    //何らかの処理
+    //    transform_.rotate_.y += 1.0f;     // 1°ずつ回転
+    //}
 
     //Debug::Log("angle = ");
     //Debug::Log(transform_.rotate_.y, true);
@@ -56,19 +53,29 @@ void Controller::Update()
     XMVECTOR vPos = XMLoadFloat3(&transform_.position_);
 
     //1フレームの移動ベクトル
-    XMVECTOR vMove{ 0.0f, 0.0f, 0.1f, 0.0f }; //奥に0.1m
+    XMVECTOR vMove1{ 0.0f, 0.0f, 0.1f, 0.0f }; //奥に0.1m
+
+    //1フレームの移動ベクトル
+    XMVECTOR vMove2{ 0.1f, 0.0f, 0.0f, 0.0f };//横に0.1m
+
+    XMVECTOR vMove3{ 0.0f, 0.1f, 0.0f, 0.0f };//縦に0.1m
 
     //transform_.rotate_.y度回転させる行列を作成
     XMMATRIX mRotY = XMMatrixRotationY(XMConvertToRadians(transform_.rotate_.y));
 
-    //移動ベクトルを変換(戦車と同じ向きに回転)させる
-    vMove = XMVector3TransformCoord(vMove, mRotY);
+    //移動ベクトルを変換させる
+    vMove1 = XMVector3TransformCoord(vMove1, mRotY);
+
+    //移動ベクトルを変換させる
+    vMove2 = XMVector3TransformCoord(vMove2, mRotY);
+
+    vMove3 = XMVector3TransformCoord(vMove3, mRotY);
 
     //Wキーが押されたら移動
     if (Input::IsKey(DIK_W))
     {
         //移動
-        vPos += vMove;
+        vPos += vMove1;
 
         //現在地をベクトルからいつものtransform_.position_に戻す
         XMStoreFloat3(&transform_.position_, vPos);
@@ -78,61 +85,76 @@ void Controller::Update()
     if (Input::IsKey(DIK_S))
     {
         //移動
-        vPos -= vMove;
+        vPos -= vMove1;
 
         //現在地をベクトルからいつものtransform_.position_に戻す
         XMStoreFloat3(&transform_.position_, vPos);
     }
 
-    if (Input::IsKeyDown(DIK_Z))
+    if (Input::IsKey(DIK_D))
     {
-        camType_++;
-        if (camType_ == CAM_TYPE_MAX)
-        {
-            camType_ = 0;
-        }
+        //何らかの処理
+        //transform_.rotate_.y += -1.0f;     // 1dd°ずつ回転
+        //移動
+        vPos += vMove2;
+
+        //現在地をベクトルからいつものtransform_.position_に戻す
+        XMStoreFloat3(&transform_.position_, vPos);
     }
 
-    switch (camType_)
+    if (Input::IsKey(DIK_A))
     {
-    case CAM_TYPE_FIXED:
-    {
-        Camera::SetTarget(XMFLOAT3(0, 0, 0));
-        Camera::SetPosition(XMFLOAT3(0, 20, -30));
-        break;
+        //何らかの処理
+        //transform_.rotate_.y += 1.0f;     // 1dd°ずつ回転
+        //移動
+        vPos -= vMove2;
+
+        //現在地をベクトルからいつものtransform_.position_に戻す
+        XMStoreFloat3(&transform_.position_, vPos);
     }
 
-    case CAM_TYPE_TPS_NO_ROT:
+    if (Input::IsKey(DIK_UP))
     {
-        Camera::SetTarget(transform_.position_);
-        XMFLOAT3 camPos = transform_.position_;
-        camPos.y += 5;
-        camPos.z -= 10;
-        Camera::SetPosition(camPos);
-        break;
+        vPos += vMove3;
+
+        XMStoreFloat3(&transform_.position_, vPos);
     }
 
-    case CAM_TYPE_TPS:
+    if (Input::IsKey(DIK_DOWN))
     {
-        Camera::SetTarget(transform_.position_);
-        XMVECTOR vCam = { 0, 5, -10, 0 };
-        vCam = XMVector3TransformCoord(vCam, mRotY);
-        XMFLOAT3 camPos;
-        XMStoreFloat3(&camPos, vPos + vCam);
-        Camera::SetPosition(camPos);
-        break;
+        vPos += vMove1;
+
+        XMStoreFloat3(&transform_.position_, vPos);
     }
 
-    case CAM_TYPE_FPS:
+    if (Input::IsKey(DIK_LEFT))
     {
-        Camera::SetPosition(transform_.position_);
-        XMFLOAT3 camTarget;
-        XMStoreFloat3(&camTarget, vPos + vMove);
-        Camera::SetTarget(camTarget);
-        break;
+        //何らかの処理
+        transform_.rotate_.y += -1.0f;     // 1dd°ずつ回転
     }
 
+    if (Input::IsKey(DIK_RIGHT))
+    {
+        //何らかの処理
+        transform_.rotate_.y += 1.0f;     // 1dd°ずつ回転
     }
+
+
+
+    /*Camera::SetTarget(transform_.position_);
+    XMFLOAT3 camPos = transform_.position_;
+    camPos.y += 5;
+    camPos.z -= 10;
+    Camera::SetPosition(camPos);*/
+
+    Camera::SetTarget(transform_.position_);
+    XMVECTOR vCam = { 0, 5, -10, 0 };
+    vCam = XMVector3TransformCoord(vCam, mRotY);
+    XMFLOAT3 camPos;
+    XMStoreFloat3(&camPos, vPos + vCam);
+    Camera::SetPosition(camPos);
+
+}
 
     //Ground* pGround = (Ground*)FindObject("Ground");    //ステージオブジェクトを探す
     //int hGroundModel = pGround->GetModelHandle();    //モデル番号を取得
@@ -148,8 +170,6 @@ void Controller::Update()
     {
     }*/
     //    transform_.position_.y = -data.dist;
-    
-}
 
 //描画
 void Controller::Draw()
