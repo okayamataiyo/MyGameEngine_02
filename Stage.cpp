652a0fefb2450fba5ea2a6c9cda497a15308c939
太陽 岡山 <220069@jc-21.jp>
2023-11-02 11:34:27 +0900

    {}の位置を統一化(Stage.cpp)

--- Stage.cpp
+++ Stage.cpp
@@ -21,12 +21,15 @@ struct Block {
 Stage::Stage(GameObject* parent)
     :GameObject(parent, "Stage")
 {
-    for (int i = 0; i < MODEL_NUM; i++) {
+    for (int i = 0; i < MODEL_NUM; i++) 
+    {
         hModel_[i] = -1;
     }
 
-    for (int x = 0; x < XSIZE; x++){
-        for (int z = 0; z < ZSIZE; z++){
+    for (int x = 0; x < XSIZE; x++)
+    {
+        for (int z = 0; z < ZSIZE; z++)
+        {
             table_[x][z].type = BLOCKTYPE::DEFAULT;
             table_[x][z].height = 0;
         }
@@ -41,7 +44,8 @@ Stage::~Stage()
 //初期化
 void Stage::Initialize()
 {
-    string modelname[] = {
+    string modelname[] = 
+    {
         "BoxDefault.fbx",
         "BoxBrick.fbx",
         "BoxGrass.fbx",
@@ -51,17 +55,21 @@ void Stage::Initialize()
     string fname_base = "assets/";
 
     //モデルデータのロード
-    for (int i = 0; i < MODEL_NUM; i++){
+    for (int i = 0; i < MODEL_NUM; i++)
+    {
         hModel_[i] = Model::Load(fname_base + modelname[i]);
         assert(hModel_[i] >= 0);
     }
 
-    /*for (int i = 0; i < MODEL_NUM; i++) {
+    /*for (int i = 0; i < MODEL_NUM; i++) 
+    {
         hModel_[i] = -1;
     }*/
     //tableにブロックのタイプをセット
-    for (int z = 0; z < ZSIZE; z++) {
-        for (int x = 0; x < XSIZE; x++) {
+    for (int z = 0; z < ZSIZE; z++) 
+    {
+        for (int x = 0; x < XSIZE; x++) 
+        {
             SetBlock(x, z, (BLOCKTYPE)(0));
             SetBlockHeight(x, z, 0);
         }
@@ -74,7 +82,8 @@ void Stage::Initialize()
 void Stage::Update()
 {
     timer_++;
-    if (Input::IsMouseButton(0) && timer_ >= 5) {
+    if (Input::IsMouseButton(0) && timer_ >= 5)
+    {
             BlockWrite();
             timer_ = 0;
     }
@@ -88,7 +97,8 @@ void Stage::BlockWrite()
     float h = (float)(Direct3D::scrHeight / 2.0f);
     //Offsetx,yは0
     //minZ = 0 maxZ = 1
-    XMMATRIX vp = {
+    XMMATRIX vp = 
+    {
         w, 0,0,0,
         0,-h,0,0,
         0, 0,1,0,
@@ -113,9 +123,12 @@ void Stage::BlockWrite()
 
     rayHit_ = false;
 
-    for (int x = 0; x < 15; x++) {
-        for (int z = 0; z < 15; z++) {
-            for (int y = 0; y < table_[x][z].height + 1; y++) {
+    for (int x = 0; x < 15; x++) 
+    {
+        for (int z = 0; z < 15; z++) 
+        {
+            for (int y = 0; y < table_[x][z].height + 1; y++) 
+            {
 
                 //⑤　②から④に向かってレイをうつ（とりあえずモデル番号はhModel_[0]）
                 RayCastData data;
@@ -130,20 +143,25 @@ void Stage::BlockWrite()
 
                 
 
-                if (data.hit && !rayHit_) {
+                if (data.hit && !rayHit_) 
+                {
                     rayHit_ = true;
                     //⑥レイが当たったらブレークポイントで止める
-                    if (controlId_ == IDC_RADIO_UP) {
+                    if (controlId_ == IDC_RADIO_UP) 
+                    {
                         table_[x][z].height++;
                         break;
                     }
-                    else if (controlId_ == IDC_RADIO_DOWN) {
-                        if (y > 0) {
+                    else if (controlId_ == IDC_RADIO_DOWN)
+                    {
+                        if (y > 0) 
+                        {
                             table_[x][z].height--;
                             break;
                         }
                     }
-                    else if (controlId_ == IDC_RADIO_CHANGE) {
+                    else if (controlId_ == IDC_RADIO_CHANGE) 
+                    {
                         SetBlock(x, z, (BLOCKTYPE)(comboId_));
                         break;
                     }
@@ -158,9 +176,12 @@ void Stage::Draw()
 {
     Transform blockTrans;
 
-    for (int x = 0; x < 15; x++){
-        for (int z = 0; z < 15; z++){
-            for (int y = 0; y < table_[x][z].height + 1; y++){
+    for (int x = 0; x < 15; x++)
+    {
+        for (int z = 0; z < 15; z++)
+        {
+            for (int y = 0; y < table_[x][z].height + 1; y++)
+            {
                 int type = table_[x][z].type;
 
                 blockTrans.position_.x = x;
@@ -186,7 +207,8 @@ void Stage::Release()
 //Stageのダイアログプロシージャ
 BOOL Stage::DialogProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    switch (msg){
+    switch (msg)
+    {
     //ダイアログが出来た時
     case WM_INITDIALOG:
         SendMessage(GetDlgItem(hDlg,IDC_RADIO_UP), BM_SETCHECK, BST_CHECKED, 0);             //ラジオボタンの初期化
@@ -228,7 +250,8 @@ void Stage::Save()
     std::stringstream oss;
 
     //OPENFILENAME構造体を初期化
-    OPENFILENAME ofn; {
+    OPENFILENAME ofn; 
+    {
         ZeroMemory(&ofn, sizeof(ofn));
         ofn.lStructSize = sizeof(OPENFILENAME);
         ofn.lpstrFilter = TEXT("マップデータ(*.map)\0*.map\0");
@@ -239,10 +262,12 @@ void Stage::Save()
     }
 
     //ファイルに保存
-    if (GetSaveFileName(&ofn)) {
+    if (GetSaveFileName(&ofn)) 
+    {
         
         std::fstream outputFile(fileName, std::ios::binary | std::ios::out);
-        for (int x = 0; x < XSIZE; x++) {
+        for (int x = 0; x < XSIZE; x++) 
+        {
             for (int z = 0; z < ZSIZE; z++) {
                 outputFile.write((char*)&table_[x][z], sizeof(Block));
 
@@ -261,7 +286,8 @@ void Stage::Load()
     std::stringstream oss;
 
     //OPENFILENAME構造体を初期化
-    OPENFILENAME ofn; {
+    OPENFILENAME ofn; 
+    {
         ZeroMemory(&ofn, sizeof(ofn));
         ofn.lStructSize = sizeof(OPENFILENAME);
         ofn.lpstrFilter = TEXT("マップデータ(*.map)\0*.map\0");
@@ -275,8 +301,10 @@ void Stage::Load()
     if (GetOpenFileName(&ofn)) {
         std::fstream inputFile(fileName, std::ios::binary | std::ios::in);
 
-        for (int x = 0; x < XSIZE; x++) {
-            for (int z = 0; z < ZSIZE; z++) {
+        for (int x = 0; x < XSIZE; x++) 
+        {
+            for (int z = 0; z < ZSIZE; z++) 
+            {
                 inputFile.read((char*)&table_[x][z], sizeof(Block));
             }
         }
@@ -314,7 +342,8 @@ void Stage::Load()
     //if (selFile == FALSE) return;
 
     //HANDLE hFile;
-    //hFile = CreateFile(
+    //hFile = CreateFile
+    // (
     //    fileName, //ファイル名
     //    GENERIC_WRITE,  //アクセスモード
     //    0,
@@ -325,8 +354,10 @@ void Stage::Load()
     //);
 
     //string data = "";
-    //for (int x = 0; x < XSIZE; x++) {
-    //    for (int z = 0; z < ZSIZE; z++) {
+    //for (int x = 0; x < XSIZE; x++)
+    //  {
+    //    for (int z = 0; z < ZSIZE; z++) 
+    // {
     //        data += to_string(table_[x][z].type) + " "
     //            + to_string(table_[x][z].height) + "\n";
     //    }
@@ -336,7 +367,8 @@ void Stage::Load()
 
     ////data.length()
     //DWORD bytes = 0;
-    //WriteFile(
+    //WriteFile
+    // (
     //    hFile,      //ファイルハンドル
     //    charData,   //保存したい文字列
     //    static_cast<DWORD>(data.length()),         //保存する文字数
